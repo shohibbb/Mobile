d9c14d03c63d77288ee2a4e4d7c27d20db65aae1
shohibb <akmalshahibmaulana@gmail.com>
2023-10-16 15:56:28 +0700

    modul 1 remake

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -1,68 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:praktikum1/Home.dart';
 import 'package:praktikum1/Search.dart';
 import 'package:praktikum1/profile.dart';
 
-class navbar extends StatefulWidget {
-  const navbar({super.key});
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
 
   @override
-  _navbarState createState() => _navbarState();
+  _NavbarState createState() => _NavbarState();
 }
 
-class _navbarState extends State<navbar> {
-  int _selected = 0;
+class _NavbarState extends State<Navbar> {
+  RxInt _selected = 0.obs;
 
-  void onTap (int index){
-    setState(() {
-      _selected = index ;
-    });
+  void onTap(int index) {
+    _selected.value = index;
   }
+
   @override
   Widget build(BuildContext context) {
-    final listpage = <Widget>[
+    final List<Widget> listpage = [
       Home(),
       const SearchBarApp(),
-      Profile()
+      Profile(),
     ];
-
-    final bottom = <BottomNavigationBarItem>[
-      const BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'home'
-      ),
-      const BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search'
-      ),
-      const BottomNavigationBarItem(
-          icon: Icon(Icons.person_2_outlined),
-          label: 'Profile'
-      ),
-      const BottomNavigationBarItem(
-          icon: Icon(Icons.history),
-          label: 'History'
-      ),
-
-    ];
-
-    final bottomNavBar = BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.black,
-      items: bottom,
-      currentIndex: _selected,
-      unselectedItemColor: Colors.grey,
-      selectedItemColor: Colors.blueAccent,
-      onTap: onTap,
-    );
 
     return Scaffold(
-      body: Center(
-        child: listpage[
-        _selected
-        ],
+      body: Obx(
+            () => IndexedStack(
+          index: _selected.value,
+          children: listpage,
+        ),
       ),
-      bottomNavigationBar: bottomNavBar,
+      bottomNavigationBar: Obx(
+            () => BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+          ],
+          currentIndex: _selected.value,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.blueAccent,
+          onTap: onTap,
+        ),
+      ),
     );
   }
 }
